1c34536b6bbf336cc49652b161f5d74ab4ed5eb5
sarakhaled95 <architectsarakhaled@outlook.com>
2024-07-11 14:58:25 +0300

    single recipe page details

--- src/data/tempDetails.js
+++ src/data/tempDetails.js
@@ -1,33 +1,16 @@
-export const recipeData = [
-    {
+export const recipeData = {
         url: "https://www.seriouseats.com/basic-rotisserie-chicken-recipe",
         label: "Rotisserie Chicken Recipe",
-        image: "https://edamam-product-images.s3.amazonaws.com/web-img/88e/88edb31264dc1e58b37c2fec3f99a244.jpg?X-Amz-Security-Token=IQoJb3JpZ2luX2VjEGYaCXVzLWVhc3QtMSJGMEQCIASa9czGsuPnr3q8ONs%2B%2BOzbNwFRvpL2Vc%2BPpCF2scpuAiBE7pr8LWM8b8%2FZyO6Cc6nNbv6zi8AcDcAsG4IvDl6%2BBSq4BQguEAAaDDE4NzAxNzE1MDk4NiIMJrMT4E9p4mSYdanxKpUFuvyY83kcR8ZQAEVGWiJVuaGuZyylabg%2BYBNh3mNviuBD%2Be8b9qynE9FHU2D2bs7eNlhLV4Wuc%2B4hmavGBKEvAYktcOdaZonfS2h0oevWe1zb4tV9B6ldQGCeo3q2LMtvCBBqh3bWXr%2B74pU%2BqG2HHfOsYuUdXR4M048%2Fbs%2FF2ldPmitF3qyYb%2FXkGB26FIIB%2FtxuSQX2ixTcUpHU%2FqM0SiMiAN%2BgD3dIn%2F%2B6JCrVpGAXIICdCLgroRsIkj%2BELTsR0JHpQbc8hc5GnorADWvjbEREh0goVX951j7R2jKOVC8dLChiUZwBr5kdw8%2BF9tSLvRYG749qrd%2Bqf36Lqgcf93M44b6PBVduft%2F32N4aOo%2FgIqwJ8RifwIcU5J6TnWAKaar1raVpcsb2WnUVgD6d5SUgltLHv9NUXj%2FgfY6GvfaUOt6t%2BTzV5WrQHIdRb1cC4oSsjSEIdpr6btl%2FDP%2FbxcLRqheuNF0vyTKpOyI4OBD2%2BMASe9dL8oWXxr6jPz82W%2F%2FG9MhUqxhwXREMLpRJpdJbjXb2Ci6rZie%2F9%2BToV%2BiXmYvG9%2Fv69EHWwCqxj8LABxWqtAR4qjUiGiy%2FHT5PBDH6O7NwvP%2B5Q0J59gQtp2tiuDZoBCMO1E%2F9XlDYOVSvoQabrbGqzSaZggn162af%2FvZinEhOfwjun53ZXVWh5IReZbXwXbLgUQy2GwfcZ7dLfCijYtdyRYd57wxcjWBZqAy%2BOhJlHdsuRcxego5%2B6SDIUrWJHQRRbFnVCqNTiBZ0BX1xFn1gYpwb37%2B5QjTEuB5mPHfVgsh8QrrH4RBdBdoOU23yCCnHlT2yFQbZVSv3zPg2%2FxFdh3MsRUp3l9Z7SCCSgRicEIxJfjNJNURgrycIIufu4zDr77S0BjqyAY4jsbzViJq8CJnZrkFavfO2DfV9ta9iMmMc7nKszYuPb%2FCQypgE1G8mjl9GW9xBw53C6JADdTN2iBidUe5%2FBgVeW4XzQ%2B%2F84czXp63LjNblsQQyBi7jHMHi2w0NNSX%2BszIOw6aEeLcU0jWtj5R29pq%2B%2FU7D2t3IPhxYhI275yrJUar1pxWrPgq94O1sfHi3SVtYHu81di25F24CUO0CH%2BYfFhB3NzOPtE5HH%2BbVnGm1Ej4%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240709T142045Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=ASIASXCYXIIFBIZKY67W%2F20240709%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=82f6a86fde70950047161b823818ff9d1cc37c3be004aeb5aac083a528f3acc0",
-        ingredientLines: {
-            0: "1 whole chicken, about 4 pounds",
-            1: "2 tablespoons kosher salt",
-            2: "2 tablespoons butter, melted"
-        },
-        mealType: {
-            0: "lunch/dinner"
-        },
-        dishType: {
-            0: "starter"
-        }
-    },
-    {
-        "uri": "https://www.bbcgoodfood.com/recipes/jamaican-jerk-chicken",
-        "label": "Jamaican jerk chicken",
-        "image": "https://edamam-product-images.s3.amazonaws.com/web-img/05b/05b69e86d6af7eb3795d6d5d5a400fc5.jpg?X-Amz-Security-Token=IQoJb3JpZ2luX2VjEGMaCXVzLWVhc3QtMSJGMEQCICqOMr5ZgEmeMoCN7tnmImpejSLf1nJabeNM0RVopFyvAiAK51ByofsG%2B163vbeWB%2F1ZziPUZvyc7dh8qEWyyZAt6iq5BQgrEAAaDDE4NzAxNzE1MDk4NiIMksV1wMEJuC088jE9KpYFAYRufNcG%2BXt5sNvvXFd2Eh8seVPWRUju%2BFVeyadAuKFbIYf3ANy18AstxpQsaEjiVasw8zzy3b0pEZBr4M2b%2Fx0HVQlNqP1JqeE%2F%2B8zOEYeJCd5bqOg5zGQUT%2BPni7%2BT80WFtLPYJd6UsqxyDBq5EdUMGHDl%2FjsIirVx8sqBzWnUAleBFstb7NtqdJweYtVKvK7IxZVQunQQTKuC%2BRZkKVvx7HAX2jk7f%2BQY8pcED0v%2FlgTMNY8BWfAZQeYwN1sruqSSZrUKN8HSe81%2FhHt%2FEMSJ7kcpiXxcsePzFBI1qYmToof2r8NDdGqyFKuH4gju8SAfDEbx5BHR87eVq5EN6YYtaIuumkKkIQWAtTDsimG7e2gBCZjlIw3yE%2Ba6qSx26ppinSinekfaDSR4%2B9pjnyBK9XU%2F2unGUtMD%2BXQdJ3L3XPpdWjjs3ZGmv0oXaEekBLUK8XNZ%2FFJNDXQ16%2F%2BoNCyJ1TGs58wkLdhnOHm94acHfWaVP%2FjvRuCxvsP0zr727nxvWH3FMdrWLgEb0tCiTSmW7EriHSWOUf3wY6hTmEDHr5cK%2F7huGnmqu%2FRCEy02PkLf%2B1P8F5sNUW6jrqRvTQBeeWhpnudCAJL0Wt4IdVlVfdsN9h6cmWmzSXkS%2B9UTDzib3m5ECxbtzmiTgmK0zPzSkMAIiFCKffMuuz84mL4m3gurrcxMf7mYlSqu6pgwdQzDmRfJKpfjaEWtjrRavehFRgddyIVSc9KHwSk89oRidlow4xmH8Z6pELIEgPIE1H24whoy9dSqkGLzW65qeHfdQm7QtsScGchG7oRSpWw5fMWvzhU7wDYHzxnstZZt%2BXbynVaMEUdp6MPpfnX5FIlq%2BdHzNsfRHuo9cm6s1domEI4sfw8wtpi0tAY6sgF8AB6%2Bf030cTuu5cPdRR%2FjEJJdTjUV5Fj1L0pR3uqqURLKaWYjmwnZ6toK%2B07ewJdP2MxcDZldL4zplR6W3k%2FnQkbP92O1rMY4mewqgjjRm2s5a4gVpIiuOddgHeQdcB%2B49gZRWjTH9a5bxydvC5WNQBD%2BDZce%2FzPBpqWWkDTf1URgmknl9sPAZxl7gfqqVPZSbQPmaNWUGgQ5iCNtMHf7V9kwMzHoiveiD6gT%2BGSSOTIF&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240709T120238Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=ASIASXCYXIIFFL6GTX35%2F20240709%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=71185d08b730ff40dff14ca510f1b76386928a12d2b6eb9015a2b3e578513daf",
-        ingredientLines: {
-            0: "2 chicken thighs",
-            1: "2 chicken legs",
-        },
-        mealType: {
-            0: "lunch/dinner"
-        },
-        dishType: {
-            0: "condiments and sauces"
-        }
-    }
-]
+        image: "https://edamam-product-images.s3.amazonaws.com/web-img/88e/88edb31264dc1e58b37c2fec3f99a244.jpg?X-Amz-Security-Token=IQoJb3JpZ2luX2VjEJL%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJGMEQCIF6r57JzNVUQbymgIF3OVM0lpqa19OxWmNdwHYKhQ%2FaFAiA9JYD9GAYajKQRe3XUxEzbZLkzN6VotooWgAkcQo9d1iq5BQhaEAAaDDE4NzAxNzE1MDk4NiIM0n8RcIWctRSahT%2F1KpYF0ydP41%2BcJpz1DjmEpxOkjptUe6PjMf4VULeU95%2Bja4PXL%2Bew3tDG%2B2wfQBn%2BNrSb6%2FywuEFcUBippkDDXEbGDwpHAD6168pLVcRfuZuXZwSqEvScASrnT8tGVIaPrtIE7YN7qRDMB0rBgjioliVJcNrlo%2F6W76RFY0BnW2YmYzuqeivbrnq39%2Fj9m4ivpQwxLV8mburnQiss2Z204pemTkUCjwokhFGXzTPGJ5OBcXCQsAi49tc8QqQTRghHcDTRn%2BEeWwjdGykBaRYtdJZjnWD8UUSyHmHx0iSeoX73mo6XRUSPYBs7Isd6J8kytqskwBn7Adce%2Fy34OThKFmCKNKZF7%2BvwOUeIJqaRy9viwx2vIwjiKkBtr5PFzlr2OgkNOOTDkBOfMctoptSBrQ5FyP7iDFdcjdOCSeuXGnmfLfQRi8ntpeyS1Y7n9a4sDfu7jQxHlC7czwNlRc%2FtgGTGzHLTPtq80XF7QKgkOHL4ZjtQCMX9WsUC42jLJR%2Fm82AlTQn5sjjNcWVyqeKrCmLK9tPPVK6gm%2BK203wFJ6KjsVtrrhJfEOwbxVhYiVvWF8iJcSHWmJw%2F689Tqv4Tk0gCgcEsSp%2BYpVB9GsqK%2F1tyYazy%2FGEKTdNDdSIsE2Y521rxnr8yJgcCbjs6JGjNww2nV7mBYs8m8LWtwiu%2FDVgUsaEzKzps2rEsm%2BEoLC6lakk8nDWBhY5%2B0muVOJp8%2FY8P5A19IpVBTAS%2FnjxZ7mh7oZGH3xdh066XjEqfSdhZ0scs4RxVDSKdFxfneyBy%2BZ%2FwakxR47drhPYmJ9z9Wl7qxw9D8AQjdI10oI5dwkRTNTmppoz291JOkLfbK6dQcXZajiZSs%2FD8ayLYcyelkhjMe23qhoxadm8wm8K%2BtAY6sgFdFI40ueW06vNZMZw62IsMKlrAbdXp6uXqkf8hyZ1DRo6%2FAl%2FAHUME5byEAg8lt2rGmoC9oxt05ry9RoI6xi7Q0pMuA%2BXk6JdeT8xGkhGDxDeW7%2FP4i81Jiq1SzywTcYHsqP7%2BkwZ8WyTGrx522F0TQMFfSyO13I569iY9xjc6tmYSMtjQgC%2FS8qQcJg9JxnjCfRcYV3osiAOf%2B5ORBPBXCJZJdBQQTw3iOGF5TTXwovE6&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240711T100733Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=ASIASXCYXIIFDZSMD5HS%2F20240711%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=4d631ccfdd3f779fff18875e733cbed61f4bcdd29267844fa2508b9af65bc986",
+        ingredientLines: [
+            "1 whole chicken, about 4 pounds",
+            "2 tablespoons kosher salt",
+            "2 tablespoons butter, melted"
+        ],
+        mealType: [
+            "lunch/dinner"
+        ],
+        dishType: [
+            "starter"
+        ]
+};
--- src/pages/SingleRecipe.js
+++ src/pages/SingleRecipe.js
@@ -1,7 +1,49 @@
 import React, { Component } from 'react';
-
+import { recipeData } from '../data/tempDetails';
+import { Link } from 'react-router-dom';
 export default class SingleRecipe extends Component {
+    constructor(props) {
+        super(props)
+        console.log(this.props)
+        const id = this.props.label;
+        this.state = {
+            recipe: recipeData,
+            id,
+            loading: false
+        }
+    }
     render() {
-        return <h3>hello from single recipe page</h3>;
+        const { url, label, image, ingredientLines } = this.state.recipe;
+        if (this.state.loading) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col-10 mx-auto col-md-6 my-5">
+                            <h2 className="title-italic text-center">loading recipe ...</h2>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+        return (
+            <div className="container my-5">
+                <div className="row">
+                    <div className="col-10 mx-auto col-md-6 my-5">
+                        <Link to="/recipes" className="btn mb-5">BACK TO SEARCH</Link>
+                        <img src={image} className="d-block w-100" style={{ maxHeight: "30rem" }} alt="recipe"></img>
+                    </div>
+                    <div className="col-10 mx-auto col-md-6 my-5">
+                        <h6 className="mx-2">{label}</h6>
+                        <a href={url} target="_blank" rel="noopener noreferrer" className="btn mx-2 mb-3">recipe url</a>
+                        <ul className="list-group my-1">
+                            <h6 className="mx-2">INGREDIENTS</h6>
+                            {ingredientLines.map((item, index)=>{
+                                return (<li key={index} className="mx-2">{item }</li>);
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
